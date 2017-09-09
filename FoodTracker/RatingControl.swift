f5d58758201d35c17e4e384658bfedcb6a5573f5
Eric Tien Chu <eric.chu1@uqconnect.edu.au>
2017-09-09 16:23:59 +1000

    Complete up to ratings control custom stack view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Andy Chu on 9/9/17.
+//  Copyright © 2017 Eric Tien Chu. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
