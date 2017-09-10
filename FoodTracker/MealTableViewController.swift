1b3ffaa4acc887e0cb9cedef23b8f905a81f9d56
Eric Tien Chu <eric.chu1@uqconnect.edu.au>
2017-09-10 16:28:26 +1000

    Custom Table View Cells, Model-View Attachment, Sample Data, Dynamic Fill

---
+++ FoodTracker/MealTableViewController.swift
@@ -0,0 +1,133 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by Andy Chu on 10/9/17.
+//  Copyright © 2017 Eric Tien Chu. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewController: UITableViewController {
+    
+    // MARK: Properties
+    var meals = [Meal]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Load the sample data
+        loadSampleMeals()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Table view cells are reused and should be dequeued using a cell identifier
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell")
+        }
+        
+        // Configure the cell...
+        // Fetches the appropriate meal from the data source layout
+        let meal = meals[indexPath.row]
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: Private Methods
+    func loadSampleMeals() {
+        var photos = [UIImage]()
+        let mealNames = ["Caprese Salad", "Chicken and Potatoes", "Pasta with Meatballs"]
+        let mealRatings = [4, 5, 3]
+        for index in 0..<3 {
+            guard let photo = UIImage(named: "meal\(index + 1)") else {
+                fatalError("Unable to instantiate image for photo\(index)")
+            }
+            photos.append(photo)
+            
+            guard let meal = Meal(name: mealNames[index], photo: photos[index], rating: mealRatings[index]) else {
+                fatalError("Unable to instantiate meal\(index)")
+            }
+            meals.append(meal)
+        }
+        
+        /*guard let meal1 = Meal(name: "Caprese Salad", photo: photos[0], rating: 4) else {
+            fatalError("Unable to instantiate meal1")
+        }
+        
+        guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photos[1], rating: 5) else {
+            fatalError("Unable to instantiate meal1")
+        }
+        
+        guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photos[2], rating: 3) else {
+            fatalError("Unable to instantiate meal1")
+        }
+        
+        meals += [meal1, meal2, meal3]*/
+    }
+
+}
