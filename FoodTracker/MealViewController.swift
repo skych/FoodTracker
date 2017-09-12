9e693acf5a064b8cdad5989cd4b6c30fe05117f4
Eric Tien Chu <eric.chu1@uqconnect.edu.au>
2017-09-12 14:23:06 +1000

    Added support to edit and delete meals. Editing based on presentation (modal VS stacked) TODO: Saving data across sessions

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -28,6 +28,13 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         // Handle the text field's user input through delegate callbacks
         nameTextField.delegate = self
         
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         // Enable the Save button only if the text field has a valid Meal name
         updateSaveButtonState()
     }
@@ -71,7 +78,16 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     // MARK: Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        // Depending on the style of presentation (modally or push presentation), this view controller needs to be dismissed in two different ways
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        } else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        } else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     
     // This method allows you to configure a view controller before it's presented
